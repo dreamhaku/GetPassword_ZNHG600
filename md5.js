6401fea59752614d9bb1dd1968a5cd49da5ed79e
dreamhaku <89404493+dreamhaku@users.noreply.github.com>
2023-07-30 22:42:26 +0800

    Update md5.js

--- md5.js
+++ md5.js
@@ -298,7 +298,7 @@ function getPassword()
       console.log(i+"/99999999");
     }
     var result = hex_md5(name);
-    if (result=="")
+    if (result=="9e04b6d766f4c868ef176dc6441c38e6")
     {
       console.log(name);
       break;
